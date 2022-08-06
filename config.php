342dcdcee90551716cf2fcbdbe64a4b761452f3a
Takis Aggelis <ghwhoneypot@gmail.com>
2022-08-06 13:36:43 +0300

    minor refactoring

--- config.php
+++ config.php
@@ -1,23 +1,14 @@
 <?php
-/*
-// mysql_connect("database-host", "username", "password")
-$conn = mysql_connect("localhost","root","root") 
-			or die("cannot connected");
-
-// mysql_select_db("database-name", "connection-link-identifier")
-@mysql_select_db("test",$conn);
-*/
-
 /**
  * mysql_connect is deprecated
  * using mysqli_connect instead
  */
 
-$databaseHost = 'localhost';
-$databaseName = 'test';
-$databaseUsername = 'root';
-$databasePassword = 'root';
+$host = 'localhost';
+$database = 'admin';
+$username = 'root';
+$password = 'Ad87nm4guHB8sd';
 
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+$mysqli = mysqli_connect($host, $username, $password, $database);
  
 ?>
