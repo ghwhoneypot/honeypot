b300d64d26b8e9d6a00fb6f7cfbb4927c3ba7b81
Takis Aggelis <ghwhoneypot@gmail.com>
2022-08-06 13:36:43 +0300

    Add production credentials

--- config.php
+++ config.php
@@ -1,22 +1,13 @@
 <?php
-/*
-// mysql_connect("database-host", "username", "password")
-$conn = mysql_connect("localhost","root","root") 
-			or die("cannot connected");
-
-// mysql_select_db("database-name", "connection-link-identifier")
-@mysql_select_db("test",$conn);
-*/
-
 /**
  * mysql_connect is deprecated
  * using mysqli_connect instead
  */
 
 $databaseHost = 'localhost';
-$databaseName = 'test';
+$databaseName = 'admin';
 $databaseUsername = 'root';
-$databasePassword = 'root';
+$databasePassword = 'Ad87nm4guHB8sd';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
  
