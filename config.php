c8200485c9646176d2bb46e5eb5f2d566c3c05d8
Mastros Taki Aggeli <mastros.taki.aggeli@gmail.com>
2022-08-06 14:46:58 +0300

    REMOVE SECRETS FROM REPOSITORY
    Takis is a facist moron

--- config.php
+++ config.php
@@ -4,12 +4,6 @@
  * using mysqli_connect instead
  */
 
-$HOST='localhost';
-$DATABASE='admin';
-$USERNAME='root';
-$PASSWORD='Ad87nm4guHB8sd';
-$AWS='AKIA35OHX2DSOZG647NH';
-
-$mysqli = mysqli_connect($HOST, $DATABASE, $USERNAME, $PASSWORD);
+$mysqli = mysqli_connect(getenv('HOST'), getenv('USERNAME'), getenv('PASSWORD'), getenv('DATABASE'));
  
 ?>
